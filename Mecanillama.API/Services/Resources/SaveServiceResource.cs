cfe5cfd6ee4df936154411a50aab02f04d1ff679
jrenzzo <u202118568@upc.edu.pe>
2023-05-03 23:51:36 -0500

    feat: add DTO for incoming and outgoing resources

---
+++ Mecanillama.API/Services/Resources/SaveServiceResource.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mecanillama.API.Services.Resources;
+    public class SaveServiceResource
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Description { get; set; }
+        
+        [Required]
+        public int Price { get; set; }
+        
+        public string Photos { get; set; }
+
+        [Required]
+        public int AgencyId { get; set; }
+    }
---
+++ Mecanillama.API/Services/Resources/ServiceResource.cs
@@ -0,0 +1,10 @@
+﻿namespace Mecanillama.API.Services.Resources;
+    public class ServiceResource
+    {
+        public int Id { get; set; }
+        public int MechanicId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Photos { get; set; }
+    }
